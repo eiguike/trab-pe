8267125778b8b4a910fc6fffbdf02cb606623257
Rick <japa.eihara@gmail.com>
2016-09-14 00:56:41 -0300

    Changed data from csv (2014 to 2013)

--- 3/trabalho.R
+++ 3/trabalho.R
@@ -1,11 +1,10 @@
 #gráfico de distribuição acumulada caso discreto - escada (R)
 dados <- read.csv(file="/home/floss/Desenvolvimento/trab-pe/3/3.csv",header=TRUE)
-plot(ecdf(dados$commits), main = 'Frequência acumulada por Commits\n2014/1-2014/3',
-     xlab = 'Commits', ylab = 'Semestre',
-     yaxt='n')
+plot(ecdf(dados$commits), main = 'Frequência acumulada por Commits\n2013/1-2013/4',
+     xlab = 'Commits', ylab = 'Semestre')
 
 #gráfico de linhas verticais
-plot(1:3,dados$commits, type='h', main='Commits acumulados entre\nos bimestres de 2014', 
+plot(1:4,dados$commits, type='h', main='Commits feitos entre\nos bimestres de 2013', 
      ylab='Commits', xlab= 'Bimestre', xaxt = 'n')
 axis(1,dados$semester)
 
