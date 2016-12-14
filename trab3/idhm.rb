ad766f907877750dcbed5c43751b895866a77a5d
Live session user <rick.eihara@gmail.com>
2016-12-14 10:02:18 +0000

    fixed scripts, now it writes the correct sql

--- trab3/idhm.rb
+++ trab3/idhm.rb
@@ -7,9 +7,6 @@ idhm = idhm.sort_alphabetical_by{|x| x[4] }
 i = 0
 
 idhm.each do |u|
-  puts "UPDATE cidades SET idh=#{UnicodeUtils.downcase(u[233].inspect.gsub("\"","").gsub(".","").gsub(",","."), nil)} WHERE codestado=#{u[1].inspect.gsub("\"","")} AND nome = #{u[4].upcase.inspect.gsub("\'","").gsub("\"","\'")};"
+  puts "UPDATE cidades SET idh=#{UnicodeUtils.downcase(u[233].inspect.gsub("\"","").gsub(".","").gsub(",","."), nil)} WHERE codestado=#{u[1].inspect.gsub("\"","")} AND nome = #{UnicodeUtils.upcase(u[4].upcase.inspect.gsub("\'","").gsub("\"","\'"))};"
   i = i + 1
 end
-
-puts i
-
--- trab3/pibm.rb
+++ trab3/pibm.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'sort_alphabetical'
+require 'unicode_utils'
 
 pibm = CSV.read('pibm.csv',{ force_quotes: false })
 pibm = pibm.sort_alphabetical_by{|x| x[4] }
@@ -8,11 +9,7 @@ i = 0
 
 pibm.each do |u|
   if u[0].eql? "2010"
-    puts "INSERT INTO CIDADES (codestado, nome, pib) VALUES(#{u[1].inspect.gsub("\"","") }, #{u[4].upcase.inspect.gsub("'","").gsub("\"","\'")}, #{u[18].inspect.gsub("\"","").gsub(".","").gsub(",",".")});"
+    puts "INSERT INTO CIDADES (codestado, nome, pib) VALUES(#{u[1].inspect.gsub("\"","")}, #{UnicodeUtils.upcase(u[4].upcase.inspect.gsub("'","").gsub("\"","\'"))}, #{u[18].inspect.gsub("\"","").gsub(".","").gsub(",",".")});"
     i = i + 1
   end
 end
-
-puts i
-
-
